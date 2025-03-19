97ce3af0608a7025b9beb8428dcd0814b15a454b
l1aav2 <c4035949@newcastle.ac.uk>
2025-03-19 20:21:58 +0000

    finish defining play() method and makes the UI more graceful and cohesive with while statements

--- Blackjack.py
+++ Blackjack.py
@@ -1,12 +1,15 @@
 import random
 import time
+import art
 
 
 def run():
-    while True:
+    go = True
+    while go:
        decision1 = input("Do you want to play a game of Black Jack? Type "
                       "'Y'/'N' ").upper()
        if decision1 == 'Y':
+           print(art.logo)
            play()
        elif decision1 == 'N':
            break
@@ -15,18 +18,19 @@ def run():
            continue
        decision2 = input("Do you want to play another game? [Y/N]").upper()
        if decision2 == 'Y':
-           while decision2:
+           while decision2 == 'Y':
             play()
             decision2 = input("Do you want to play another game? [Y/N]").upper()
-
-
        elif decision2 == 'N':
          break
        else:
-        print("invalid input. Please type 'Y' or 'N'")
+        while decision2 != 'Y' or 'N':
+            print("invalid input. Please type 'Y' or 'N'")
+            time.sleep(0.3)
+            decision2 = input(
+                "Do you want to play another game? [Y/N]").upper()
 
 
-  #continue
 
 
 def selection():
@@ -45,45 +49,70 @@ def play():
         count += 1
     print(f'your cards: [{[card for card in player_hand]}]')
     print(f"dealer's  first card: {dealer_hand[0]}")
-    try:
-        decision2 = input("Type 'y' to hit, type 'n' to pass:").upper()
-    except :
-        ValueError ("Please type 'y' or 'n'")
-
+    decision2 = input("Type 'y' to hit, type 'n' to pass:").upper()
 
     if decision2 == 'Y':
         player_hand.append(selection())
 
-    player_tot = sum(player_hand)
+
+
+    if dealer_hand[0]+dealer_hand[1] < 16:
+        dealer_hand.append(selection())
+
     dealer_tot = sum(dealer_hand)
+    player_tot = sum(player_hand)
     player_prox = abs(21 - player_tot)
     dealer_prox = abs(21 - dealer_tot)
-    if player_prox < dealer_prox and player_tot <= 21 and dealer_tot<= 21 :
-        print(f'your cards: [{[card for card in player_hand]}]')
-        print(f"Dealer's cards:[{[card for card in dealer_hand]}]")
-        time.sleep(0.5)
-        print("Dealer's gone bust !you win!!")
-    elif dealer_prox  < player_prox and player_tot <= 21 and dealer_tot<= 21:
-        print(f'your cards: [{[card for card in player_hand]}]')
-        print(f"Dealer's cards:[{[card for card in dealer_hand]}]")
-        time.sleep(0.5)
-        print("You've gone bust! Dealer wins!!")
-    else:
-        highest = max(player_prox,dealer_prox)
-        if highest == player_prox and player_tot <= 21 and dealer_tot<= 21:
-            print(f'your cards: [{[card for card in player_hand]}]')
-            print(f"Dealer's cards:[{[card for card in dealer_hand]}]")
+    if player_tot <= 21 and dealer_tot <= 21 :
+        if player_prox < dealer_prox:
+            print(f'your cards: {[card for card in player_hand]}')
+            print(f"Dealer's cards:{[card for card in dealer_hand]}")
             time.sleep(0.5)
-            print("You Win!!")
-        elif highest == dealer_prox and player_tot <= 21 and dealer_tot <=21:
-            print(f'your cards: [{[card for card in player_hand]}]')
-            print(f"Dealer's cards:[{[card for card in dealer_hand]}]")
+            print("Dealer's gone bust !you win!!")
+        elif dealer_prox < player_prox :
+            print(f'your cards: {[card for card in player_hand]}')
+            print(f"Dealer's cards:{[card for card in dealer_hand]}")
             time.sleep(0.5)
-            print("Dealer Wins")
+            print("You've gone bust! Dealer wins!!")
+        elif player_prox == dealer_prox and player_tot :
+            print(f'your cards: {[card for card in player_hand]}')
+            print(f"Dealer's cards:{[card for card in dealer_hand]}")
+            time.sleep(0.5)
+            print("It's a draw!!")
         else:
-            print("idk who wins, Damn!")
+            highest = max(player_prox,dealer_prox)
+            if highest == player_prox and player_tot <= 21 and dealer_tot<= 21:
+                print(f'your cards: {[card for card in player_hand]}')
+                print(f"Dealer's cards:{[card for card in dealer_hand]}")
+                time.sleep(0.5)
+                print("You Win!!")
+            elif highest == dealer_prox and player_tot <= 21 and dealer_tot <=21:
+                print(f'your cards: {[card for card in player_hand]}')
+                print(f"Dealer's cards:{[card for card in dealer_hand]}")
+                time.sleep(0.5)
+                print("Dealer Wins")
+            else:
+                print("idk who wins, Damn!")
+                print(f'your cards: {[card for card in player_hand]}')
+                print(f"Dealer's cards:{[card for card in dealer_hand]}")
+    elif player_tot > 21 and dealer_tot <= 21 :
+        print("you've gone bust! Dealer wins!")
+        print(f'your cards: {[card for card in player_hand]}')
+        print(f"Dealer's cards:{[card for card in dealer_hand]}")
+    elif dealer_tot > 21 and player_tot <= 21:
+        print(f'your cards: {[card for card in player_hand]}')
+        print(f"Dealer's cards:{[card for card in dealer_hand]}")
+        print("Dealer has gone bust! You win!")
+    else:
+        print("you've both gone bust! Nobody wins! ")
 
 
+#TO DO:
+#draw case: when the player and the dealer have the same amount in their
+# hand then they draw
+#if the dealer's total is less than 16 they have to pick up another card
+#go over the logic of the game because sometimes the dealer is supposed to
+# win either loses or draws
 
 
 
